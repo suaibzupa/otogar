7b8d88c051ad3ff199880ab564a4ba9825d991fb
Shuaib <zengo@itu.edu.tr>
2016-11-02 20:01:18 +0300

    slide

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -8,48 +8,28 @@
                 <div id="myCarousel" class="carousel slide" data-ride="carousel">
                     <!-- Indicators -->
                     <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                        @for($i = 0; $i < sizeof(json_decode($carData['images'])); $i++)
+
+                        <li data-target="#myCarousel" data-slide-to="{$i}" class="active"></li>
+
+                        @endfor
                     </ol>
 
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <a href="#"><img   src="{{"/assets/uploads/".json_decode($carData['images'])[0]}}" width="600px" height="600px" > </a>
-                            <div class="carousel-caption">
-                                <h3>Chania</h3>
-                                <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+
+                        @for($i = 0; $i < sizeof(json_decode($carData['images'])); $i++)
+                            <div class="item {{$i == 0 ? 'active' : ''}}">  <!-- Birince index'teki slide'i aktif yapar -->
+                                <a href="#"><img   src="{{"/assets/uploads/".json_decode($carData['images'])[$i]}}" > </a> <!-- Dinamik olarak index'e gore doner -->
+                                <div class="carousel-caption">
+                                    <h3>Chania</h3> <!-- Bunu da dinamic getir DB'den -->
+                                    <p>The atmosphere in Chania has a touch of Florence and Venice.</p> <!-- Burayi dinamic getir db'den -->
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="item">
-                            <a href="#"><img   src="{{"/assets/uploads/".json_decode($carData['images'])[0]}}" > </a>
-                            <div class="carousel-caption">
-                                <h3>Chania</h3>
-                                <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-                            </div>
-                        </div>
-
-                        <div class="item">
-                            <a href="#"><img   src="{{"/assets/uploads/".json_decode($carData['images'])[0]}}" > </a>
-                            <div class="carousel-caption">
-                                <h3>Flowers</h3>
-                                <p>Beatiful flowers in Kolymbari, Crete.</p>
-                            </div>
-                        </div>
-
-                        <div class="item">
+                        @endfor
 
 
-                            <a href="#"><img   src="{{"/assets/uploads/".json_decode($carData['images'])[1]}}" > </a>
 
-                            <div class="carousel-caption">
-                                <h3>Flowers</h3>
-                                <p>Beatiful flowers in Kolymbari, Crete.</p>
-                            </div>
-                        </div>
                     </div>
 
                     <!-- Left and right controls -->
@@ -62,6 +42,40 @@
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
+
+
+
+
+
+                <div class="row" >
+
+                    <br>
+                    <div  class=" col-sm-9">
+                        </div>
+
+                    <div  class=" col-sm-3" >
+                        <button type="button" class="btn btn-primary">Like</button>
+
+
+                        <hr>
+                        <h2 class="text-right" style="background-color: #3FB8AF">{{$carData['vendor']}} </h2>
+
+                        <hr>
+                        <h2 class="text-right" style="background-color: #3FB8AF" style="color: red">{{$carData['price']}} TL</h2>
+
+                        <hr>
+
+
+                    </div>
+
+                </div>
+
+
+
+
+
+
+
 
             </div>
 
@@ -90,6 +104,15 @@
                             <li class="clearfix">(Karen) The size of this jacket was a little larger.. <i class="fa fa-star fa-2x yellow pull-right"></i></li>
                             <li class="clearfix">(CAS) I love this jacket. I purchased this as part..  <i class="fa fa-star fa-2x yellow pull-right"></i><i class="fa fa-star fa-2x yellow pull-right"></i></li>
                             <li class="clearfix">(William D.) Great value with cool style. If you want.. <i class="fa fa-star fa-2x yellow pull-right"></i></li>
+
+
+                            <li class="clearfix">(Mike R.) I bought this last month before a.. <i class="fa fa-star fa-2x yellow pull-right"></i></li>
+                            <li class="clearfix">(Karen) The size of this jacket was a little larger.. <i class="fa fa-star fa-2x yellow pull-right"></i></li>
+                            <li class="clearfix">(CAS) I love this jacket. I purchased this as part..  <i class="fa fa-star fa-2x yellow pull-right"></i><i class="fa fa-star fa-2x yellow pull-right"></i></li>
+                            <li class="clearfix">(William D.) Great value with cool style. If you want.. <i class="fa fa-star fa-2x yellow pull-right"></i></li>
+
+
+
                         </ul>
 
                     </div>
@@ -105,26 +128,7 @@
         </div>
 
 
-                <div class="row" >
 
-
-                    <div  class=" col-sm-7 ">
-                    </div>
-                    <div  class=" col-sm-3 left">
-                        <button type="button" class="btn btn-primary">Like</button>
-
-                    </div>
-                        <div  class=" col-sm-2 right ">
-
-                            <hr>
-                            <h2 class="text-right" style="background-color: #3FB8AF">{{$carData['vendor']}} </h2>
-
-                            <hr>
-                            <h2 class="text-right" style="background-color: #3FB8AF" style="color: red">{{$carData['price']}} TL</h2>
-
-                            <hr>
-                        </div>
-                </div>
 
     </div>
 @endsection
